9525371a194907259c9c5d8e68066df48982806f
Alex Bibiano <alex@bibiano.es>
2012-04-15 19:32:55 +0200

    Moved files

--- w1e1.rb
+++
@@ -1,8 +0,0 @@
-y = false
-z = true
-x = y or z    # assignment has higher precedence than or
-puts x        # prints false
-(x = y) or z  # brackets give higher precedence to the asignment
-puts x        # prints false
-x = (y or z)  # brackets give higher precedence to the or
-puts x        # prints true
