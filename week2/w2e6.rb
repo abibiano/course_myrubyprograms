f57944d59c5a3d13dbe16bf6bf0a700deb3c9bbc
Alex Bibiano <alex@bibiano.es>
2012-04-24 11:35:38 +0200

    week2

--- week2/w2e6.rb
+++ week2/w2e6.rb
@@ -1,14 +1,14 @@
 # c. In the following Ruby code, x gets the value nil and pqr remains an 
 # undefined local variable. Why?
 #
-# if false
-#  x = pqr
-# end
-# puts x
-# puts pqr
-
-Variables are created and values assigned to them by assignment expressions
-therefore x is created on the assigment statment x = pqr but as this statment
-never executs the value for the variable is nil
-
-pqr has no assigment statment, therefore it's not defined
+ if false
+  x = pqr
+ end
+ puts x
+ puts pqr
+# 
+# Variables are created and values assigned to them by assignment expressions
+# therefore x is created on the assigment statment x = pqr but as this statment
+# never executs the value for the variable is nil
+# 
+# pqr has no assigment statment, therefore it's not defined
