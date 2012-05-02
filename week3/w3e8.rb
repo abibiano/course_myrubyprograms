d2e712c7e9f3e78c504f3ff9cf537e45ddccc4b2
Alejandro Bibiano <alex@bibiano.es>
2012-05-02 17:19:19 +0200

    commit

--- week3/w3e8.rb
+++ week3/w3e8.rb
@@ -6,7 +6,7 @@
 def calculate_value(number, multiple_text_hash = {:Fizz => 3 , :Buzz => 5 })
   output = ''
   multiple_text_hash.each {|key, value| output += key.to_s if number % value == 0}
-  output.size.zero? ? number : output
+  output.empty? ? number : output
 end
 
 (1..100).each {|number| puts calculate_value(number, {Fizz: 3 , Buzz: 5, Dazz: 7, Kezz: 11})}
