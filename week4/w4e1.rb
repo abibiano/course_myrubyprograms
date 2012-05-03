23320e02cac3b40ea82a7357d93c7478ee9dbd6f
Alex Bibiano <alex@bibiano.es>
2012-05-03 23:05:17 +0200

    Commit

---
+++ week4/w4e1.rb
@@ -0,0 +1,28 @@
+#  1. Write a class called Dog, that has name as an
+#  instance variable and the following methods:
+#  bark(), eat(), chase_cat()
+#  I shall create the Dog object as follows:
+#  d = Dog.new('Leo')
+
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  
+  def bark
+  end
+  
+  def eat
+  end
+  
+  def chase_cat
+  end
+  
+  def to_s
+    @name
+  end
+end
+
+d = Dog.new('Leo')
+puts d
+puts d.eat
